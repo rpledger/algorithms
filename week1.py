cacea4f735fca844d3bbd10e059a977abbdebf91
Rebecca Pledger <rebeccapledger14@gmail.com>
2017-02-17 00:06:51 -0600

    Implement sort and counting inversions

--- week1.py
+++ week1.py
@@ -29,6 +29,7 @@ def merge_and_calc_split_inversions(B, C, s):
 	i = 0
 	j = 0
 	D = []
+	count = 0
 	for k in range(0,s):
 		if i >= np.size(B):
 			D = np.insert(D, k, C[j])
@@ -42,19 +43,26 @@ def merge_and_calc_split_inversions(B, C, s):
 		elif C[j] < B[i]:
 			D = np.insert(D, k, C[j])
 			j+=1
-	return D, 0
+			count+=np.size(B)-i
+	return D, count
 
 def sort_and_calc_inversions(A, s):
 	# Base Case
+	count = 0
 	if s == 1:
 		return A, 0
+	elif s == 2:
+		if A[1] < A[0]:
+			A = np.array([A[1], A[0]])
+			count = 1
+		return A, count
 	else:
 		# Divide
 		B, C = np.array_split(A, 2)
 
 		# Conquer
-		B,x = sort_and_calc_inversions(B, s/2)
-		C, y = sort_and_calc_inversions(C, s/2)
+		B,x = sort_and_calc_inversions(B, np.size(B))
+		C, y = sort_and_calc_inversions(C, np.size(C))
 
 		# Merge
 		D, z= merge_and_calc_split_inversions(B, C, s)
@@ -65,5 +73,6 @@ samples, file = get_args()
 A = read_samples(samples, file)
 print "Input array: {}".format(A)
 A_sort, inversions = sort_and_calc_inversions(A, samples)
-print A_sort
+print "Sorted Array: {}".format(A_sort)
+print "Number of Inversions: {}".format(inversions)
 
