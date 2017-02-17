8daf8df56de71ae56737fed6f648ea0d99e967b3
Rebecca Pledger <rebeccapledger14@gmail.com>
2017-02-16 21:46:43 -0600

    Successfully implemented argparse

--- week1.py
+++ week1.py
@@ -1,13 +1,19 @@
 import argparse
 
-parser = argparse.ArgumentParser(description='Find inversion in a set of samples from a file')
-parser.add_argument('samples', metavar='SAMPLES', type=int, nargs=1,
-					help='number of input samples from file')
-parser.add_argument('file', metavar='FILE', type=argparse.FileType('r'), nargs=1,
-					help='file to read samples from')
+def get_args():
+	parser = argparse.ArgumentParser(description='Find inversion in a set of samples from a file')
+	parser.add_argument('samples', metavar='SAMPLES', type=int,
+						help='number of input samples from file')
+	parser.add_argument('file', metavar='FILE', type=argparse.FileType('r'),
+						help='file to read samples from')
 
-args = parser.parse_args()
-print args
+	args = parser.parse_args()
+	samples = args.samples
+	file = args.file
+	return samples, file
+
+
+samples, file = get_args()
 #while True:
 #	try: 
 #		samples = int(raw_input("How many inputs?: "))
@@ -15,11 +21,8 @@ print args
 #	except ValueError:
 #		print("Oops! That was not a valid number. Try again...")
 #
-file_name = input("File name?: ")
-f = open(file_name, 'r')
-if not f:
-	print "Not a readable file..."
-	exit
+#file_name = input("File name?: ")
+#f = open(file_name, 'r')
 
-print "Reading {} inputs from file {}".format(samples, file_name)
+print "Reading {} inputs from file {}".format(samples, file.name)
 
