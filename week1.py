4cbaababf5d4367818e55280e8ff0cb13721f6a3
Rebecca Pledger <rebeccapledger14@gmail.com>
2017-02-16 21:24:38 -0600

    Use argparse to add samples and file input

--- week1.py
+++ week1.py
@@ -1,10 +1,20 @@
-while True:
-	try: 
-		samples = int(raw_input("How many inputs?: "))
-		break
-	except ValueError:
-		print("Oops! That was not a valid number. Try again...")
+import argparse
 
+parser = argparse.ArgumentParser(description='Find inversion in a set of samples from a file')
+parser.add_argument('samples', metavar='SAMPLES', type=int, nargs=1,
+					help='number of input samples from file')
+parser.add_argument('file', metavar='FILE', type=argparse.FileType('r'), nargs=1,
+					help='file to read samples from')
+
+args = parser.parse_args()
+print args
+#while True:
+#	try: 
+#		samples = int(raw_input("How many inputs?: "))
+#		break
+#	except ValueError:
+#		print("Oops! That was not a valid number. Try again...")
+#
 file_name = input("File name?: ")
 f = open(file_name, 'r')
 if not f:
