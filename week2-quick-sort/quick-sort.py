6c7dc6daddc1b49334e850f6f02612c52cc6ac4a
Rebecca Pledger <rebeccapledger14@gmail.com>
2017-03-01 22:13:19 -0600

    Fix indent issue and add user input for sample size

--- week2-quick-sort/quick-sort.py
+++ week2-quick-sort/quick-sort.py
@@ -1,26 +1,27 @@
 comparision_count = 0
 
 def read_samples(num, file):
-	print "Reading inputs from file {}\n".format(file)
+	f = open(file, 'r')
+	print "Reading {} inputs from file {}".format(num, file)
 	A =[]
 	for s in range(0,num):
 		try:
-			sample = int(file.readline())
+			sample = int(f.readline())
 			A.append(sample)
 		except(ValueError):
 			print "Sample ({}) not an interger".format(sample)
-			file.close()
+			f.close()
 			exit()
-	file.close()
+	f.close()
 	return A
 
-def choose_pivot_first(A, l, r)
+def choose_pivot_first(A, l, r):
 	p = 0
 	if p != 0:
 		A[l], A[p] = A[p], A[l]
 	return A
 
-def partition(A, l, r)
+def partition(A, l, r):
 	p = A[l]
 	i = l + 1
 	for j in range (l+1, r):
@@ -29,9 +30,10 @@ def partition(A, l, r)
 			i = i + 1
 	return A
 
-def quick_sort(A, l, r)
-    comparision_count = comparision_count + r - l - 1
-	if num = 1 return A
+def quick_sort(A, l, r):
+	comparision_count = comparision_count + r - l - 1
+	if num == 1:
+		return A
 	A = choose_pivot_first(A, l, r)
 	partition(A, l, r)
 	quick_sort(A, l, p)
@@ -39,5 +41,7 @@ def quick_sort(A, l, r)
 
 
 file = "../week1-merge-sort/test.txt"
-A = read_samples(5, file)
-A_sorted, calcs = QuickSort(A, 0, 4)
+num = int(raw_input("How many samples?: "))
+A = read_samples(num, file)
+print A
+#A_sorted, calcs = QuickSort(A, 0, 4)
