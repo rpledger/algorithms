ac9eb687c13added1945a952f70fe841616da6fc
Rebecca Pledger <rebeccapledger14@gmail.com>
2017-03-29 22:12:13 -0500

    Clean up prints

--- week3-min-cut/min_cut.py
+++ week3-min-cut/min_cut.py
@@ -13,26 +13,18 @@ def read_input_data(file, size):
 			line_count -= 1
 			if line_count == 0:
 				break
-	#print len(adjacency)
 	return adjacency
 
 def remove_self_loops(super_node, non_super_node, adj):
-	#print adj
 	if super_node in adj:
-		#print "ADJ0: {}".format(adj)
 		adj[:] = [a for a in adj if a != super_node]
-		#adj.remove(super_node)
-		#print "ADJ1: {}".format(adj)
 	if non_super_node in adj:
-		#adj.remove(non_super_node)
 		adj[:] = [a for a in adj if a != non_super_node]
-		#print "ADJ2: {}".format(adj)
 	return adj
 
 def edge_contraction(v, e, graph):
 	super_node =  min(int(v), int(e))
 	non_super_node = max(int(v), int(e))
-	#print "Super: {}, Non-Super: {}".format(super_node, non_super_node)
 	for adj in graph[non_super_node]:
 		graph[super_node].append(adj)
 	graph.pop(non_super_node)
@@ -40,8 +32,6 @@ def edge_contraction(v, e, graph):
 		if non_super_node in graph[vertex]:
 			graph[vertex] = [x if x != non_super_node else super_node for x in graph[vertex]]
 	remove_self_loops(super_node, non_super_node, graph[super_node])
-	#print "V: {}, E: {}".format(v,e)
-	#print graph
 	return graph
 
 def choose_random_vertex(seed, keys):
@@ -57,13 +47,11 @@ def choose_random_edge(seed, adj_list):
 
 def minimum_cut(seed, graph):
 	global count
-	#print count
 	count+=1
 	if len(graph) == 2:
 		return len(graph[list(graph.keys())[0]])
 	mc = 0
 	# Choose random vertex
-	#print graph
 	v = choose_random_vertex(seed, graph.keys())
 	# Choose random edge from that list
 	e = choose_random_edge(seed, graph[v])
@@ -76,5 +64,4 @@ def minimum_cut(seed, graph):
 
 if __name__ == '__main__':
 	graph = read_input_data('min_cut_data.txt', 200)
-	#for i in range(0, 30):
 	print "MIN CUT: {}".format(minimum_cut(11, graph))
