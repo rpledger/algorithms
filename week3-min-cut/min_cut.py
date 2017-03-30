8ef971c1f315b457270c65b67bd0a7a455d60b82
Rebecca Pledger <rebeccapledger14@gmail.com>
2017-03-29 21:05:36 -0500

    Convert graph to ints instead of characters

--- week3-min-cut/min_cut.py
+++ week3-min-cut/min_cut.py
@@ -1,11 +1,14 @@
 import random
 
+count = 0
+
 def read_input_data(file, size):
 	adjacency = {}
 	line_count = size
 	with open(file, 'r') as f:
 		for line in f:
 			adj = line.split()
+			adj = [int(a) for a in adj]
 			adjacency[adj[0]] = adj[1:]
 			line_count -= 1
 			if line_count == 0:
@@ -14,7 +17,7 @@ def read_input_data(file, size):
 	return adjacency
 
 def remove_self_loops(super_node, non_super_node, adj):
-	print adj
+	#print adj
 	if super_node in adj:
 		adj.remove(super_node)
 	if non_super_node in adj:
@@ -22,18 +25,24 @@ def remove_self_loops(super_node, non_super_node, adj):
 	return adj
 
 def edge_contraction(v, e, graph):
-	super_node = min(v, e)
-	non_super_node = max(v,e)
+	super_node =  min(int(v), int(e))
+	non_super_node = max(int(v), int(e))
+	print "Super: {}, Non-Super: {}".format(super_node, non_super_node)
 	for adj in graph[non_super_node]:
 		graph[super_node].append(adj)
 	graph.pop(non_super_node)
+	for vertex in graph:
+		if non_super_node in graph[vertex]:
+			graph[vertex].remove(non_super_node)
 	remove_self_loops(super_node, non_super_node, graph[super_node])
+	print "V: {}, E: {}".format(v,e)
+	print graph
 	return graph
 
 def choose_random_vertex(seed, size):
 	random.seed(seed)
 	vertex = random.randint(1, size)
-	return str(vertex)
+	return vertex
 
 def choose_random_edge(seed, adj_list):
 	random.seed(seed)
@@ -41,15 +50,24 @@ def choose_random_edge(seed, adj_list):
 	return edge
 
 def min_cut(seed, graph):
+	global count
+	print count
+	count+=1
+	if len(graph) == 2:
+		return len(graph[list(graph.keys())[0]])
+	mc = 0
 	# Choose random vertex
+	print graph
 	v = choose_random_vertex(seed, len(graph))
 	# Choose random edge from that list
 	e = choose_random_edge(seed, graph[v])
 	# Do Edge Contraction
 	graph = edge_contraction(v, e, graph)
-	# Remove self loops
-
+	# Recurive call
+	mc = min_cut(seed, graph)
+	print graph
+	return mc
 
 if __name__ == '__main__':
 	graph = read_input_data('test.txt', 3)
-	#min_cut(graph)
+	print min_cut(graph)
--- week3-min-cut/min_cut_test.py
+++ week3-min-cut/min_cut_test.py
@@ -6,72 +6,101 @@ class TestMinCutMethods(unittest.TestCase):
 
 	def test_read_input_data(self):
 		graph1 = {
-			'1': ['2','3'],
-			'2': ['1'],
-			'3': ['1']
+			1: [2,3],
+			2: [1],
+			3: [1]
 		}
 		self.assertEqual(graph1, min_cut.read_input_data('test.txt', 3))
 
 	def test_read_input_data_bad_num(self):
 		graph2 = {
-			'1': ['2','3'],
-			'2': ['1'],
+			1: [2,3],
+			2: [1],
 		}
 		self.assertEqual(graph2, min_cut.read_input_data('test.txt', 2))
 
 	def test_random_vertex_seed_0(self):
 		graph = min_cut.read_input_data('test.txt', 3)
-		self.assertEqual('3', min_cut.choose_random_vertex(0, len(graph)))
+		self.assertEqual(3, min_cut.choose_random_vertex(0, len(graph)))
 
 	def test_random_vertex_seed_1(self):
 		graph = min_cut.read_input_data('test.txt', 3)
-		self.assertEqual('1', min_cut.choose_random_vertex(1, len(graph)))
+		self.assertEqual(1, min_cut.choose_random_vertex(1, len(graph)))
 
 	def test_choose_random_edge_seed_0(self):
 		graph = min_cut.read_input_data('test.txt', 3)
 		v = min_cut.choose_random_vertex(0, len(graph))
 		print graph
-		self.assertEqual('1', min_cut.choose_random_edge(0, graph[str(v)]))
+		self.assertEqual(1, min_cut.choose_random_edge(0, graph[v]))
 
 	def test_choose_random_edge_seed_1(self):
 		graph = min_cut.read_input_data('test.txt', 3)
 		v = min_cut.choose_random_vertex(1, len(graph))
 		print graph
-		self.assertEqual('2', min_cut.choose_random_edge(1, graph[str(v)]))
+		self.assertEqual(2, min_cut.choose_random_edge(1, graph[v]))
 
 	def test_choose_random_edge_seed_2(self):
 		graph = min_cut.read_input_data('test.txt', 3)
 		v = min_cut.choose_random_vertex(2, len(graph))
 		print graph
-		self.assertEqual('1', min_cut.choose_random_edge(2, graph[str(v)]))
+		self.assertEqual(1, min_cut.choose_random_edge(2, graph[v]))
+
+	def test2_choose_random_edge_seed_0(self):
+		graph = min_cut.read_input_data('test2.txt', 10)
+		v = min_cut.choose_random_vertex(2, len(graph))
+		print graph
+		self.assertEqual(8, min_cut.choose_random_edge(2, graph[v]))
 
 	def test_edge_contraction_seed_0(self):
 		graph_test = {
-			'1': ['2'],
-			'2': ['1']
+			1: [2],
+			2: [1]
 		}
 		graph = min_cut.read_input_data('test.txt', 3)
 		v = min_cut.choose_random_vertex(0, len(graph))
-		e = min_cut.choose_random_edge(0, graph[str(v)])
+		e = min_cut.choose_random_edge(0, graph[v])
+		self.assertEqual(graph_test, min_cut.edge_contraction(v, e, graph))
+
+
+	def test2_edge_contraction_seed_0(self):
+		graph_test = {
+			1: [2],
+			2: [1]
+		}
+		graph = min_cut.read_input_data('test2.txt', 10)
+		v = min_cut.choose_random_vertex(0, len(graph))
+		e = min_cut.choose_random_edge(0, graph[v])
 		self.assertEqual(graph_test, min_cut.edge_contraction(v, e, graph))
 
 	def test_edge_contraction_seed_1(self):
 		graph_test = {
-			'1': ['3'],
-			'3': ['1']
+			1: [3],
+			3: [1]
 		}
 		graph = min_cut.read_input_data('test.txt', 3)
 		v = min_cut.choose_random_vertex(1, len(graph))
-		e = min_cut.choose_random_edge(1, graph[str(v)])
+		e = min_cut.choose_random_edge(1, graph[v])
 		self.assertEqual(graph_test, min_cut.edge_contraction(v, e, graph))
 
 	def test_remove_self_loop(self):
-		graph = ['1', '2', '1']
-		graph_test = ['2', '1']
+		graph = [1, 2, 1]
+		graph_test = [2, 1]
 		#graph = min_cut.read_input_data('test.txt', 3)
 		#v = min_cut.choose_random_vertex(1, len(graph))
 		#e = min_cut.choose_random_edge(1, graph[str(v)])
-		self.assertEqual(graph_test, min_cut.remove_self_loops('1', '3', graph))
+		self.assertEqual(graph_test, min_cut.remove_self_loops(1, 3, graph))
+
+	def test_min_cut_seed_0(self):
+		graph = min_cut.read_input_data('test.txt', 3)
+		self.assertEqual(1, min_cut.min_cut(0, graph))
+
+	def test_min_cut_seed_1(self):
+		graph = min_cut.read_input_data('test.txt', 3)
+		self.assertEqual(1, min_cut.min_cut(1, graph))
+
+	# def test2_min_cut_seed_0(self):
+	# 	graph = min_cut.read_input_data('test2.txt', 10)
+	# 	self.assertEqual(1, min_cut.min_cut(0, graph))
 
 if __name__ == '__main__':
     unittest.main()
